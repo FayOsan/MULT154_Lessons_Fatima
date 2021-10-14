0f091dad0e33eaeed0fe12c9c5b90378add03a6f
FayOsan <fayosan15@gmail.com>
2021-10-14 12:54:55 -0700

    Debugging and Profiling Lesson (Gizmos)

--- Assets/Scripts/Frog/PlayerMovement.cs
+++ Assets/Scripts/Frog/PlayerMovement.cs
@@ -43,6 +43,16 @@ public class PlayerMovement : NetworkBehaviour
 
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(transform.position, direction * 10);
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawRay(transform.position, rbPlayer.velocity * 5);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawCube(transform.position, new Vector3(6, 6, 6));
+    }
+
     private void FixedUpdate()
     {
         if (!isLocalPlayer)
@@ -70,6 +80,7 @@ public class PlayerMovement : NetworkBehaviour
             index++;
         }
         rbPlayer.MovePosition(spawnPoints[index].transform.position);
+        rbPlayer.velocity = Vector3.zero;
     }
 
 
